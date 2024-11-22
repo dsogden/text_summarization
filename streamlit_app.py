fc1221c44abdfc355cad9c80b48f3b2a447760db
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-11-22 20:51:56 +0000

    fixing errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,19 @@
 import streamlit as st
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+import torch
 
-tokenizer = T5Tokenizer.from_pretrained("google-t5/t5-small")
-model = T5ForConditionalGeneration.from_pretrained("google-t5/t5-small")
-
-def get_keywords(
-        model: T5ForConditionalGeneration, input_text: str
-    ) -> str:
-    """
-    Returns the number of top words to be summarized from the text.
-    """
-    input_text = "Your book description here"
-    input_ids = tokenizer.encode("summarize: " + input_text, return_tensors="pt")
-    outputs = model.generate(
-        input_ids, max_length=150, min_length=50, num_beams=4, early_stopping=True
-    )
-    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return summary
+# def get_keywords(
+#         model: T5ForConditionalGeneration, input_text: str
+#     ) -> str:
+#     """
+#     Returns the number of top words to be summarized from the text.
+#     """
+#     input_text = "Your book description here"
+#     input_ids = tokenizer.encode("summarize: " + input_text, return_tensors="pt")
+#     outputs = model.generate(
+#         input_ids, max_length=150, min_length=50, num_beams=4, early_stopping=True
+#     )
+#     summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+#     return summary
 
 st.title("Text Summarizer")
 st.write("Enter a text below to summarize the text:")
