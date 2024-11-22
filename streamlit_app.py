5c3492dd8135526c8746c4b48727eb561d98cba9
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-11-22 20:46:56 +0000

    exploring basic functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+tokenizer = T5Tokenizer.from_pretrained('t5-large')
+model = T5ForConditionalGeneration.from_pretrained('t5-large')
+
+def get_keywords(
+        model: TT5ForConditionalGeneration, input_text: str
+    ) -> str:
+    """
+    Returns the number of top words to be summarized from the text.
+    """
+    input_text = "Your book description here"
+    input_ids = tokenizer.encode("summarize: " + input_text, return_tensors="pt")
+    outputs = model.generate(
+        input_ids, max_length=150, min_length=50, num_beams=4, early_stopping=True
+    )
+    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return summary
+
+st.title("Text Summarizer")
+st.write("Enter a text below to summarize the text:")
+user_input = st.text_area('Text to analyze', placeholder="Type something...")
+
